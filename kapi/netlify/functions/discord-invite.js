e1634ae3072309784dd91b40bc0de863d78bfd15
ZarekScott <l.zarek@yahoo.com>
2025-09-18 06:30:25 -0500

    Delete kapi directory

--- kapi/netlify/functions/discord-invite.js
+++
@@ -1,5 +0,0 @@
-export const config = { path: "/.netlify/functions/discord-invite" };
-export default async (req, ctx) => {
-  const url = process.env.DISCORD_INVITE_URL || "#";
-  return new Response(null, { status: 302, headers: { Location: url } });
-};
